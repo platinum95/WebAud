ace5d0541553c769c432b493dcb5aead2c79c71f
Peter Mullen <platinum95@gmail.com>
2019-01-12 17:44:15 +0000

    Working (sorta) and add audio

--- WebAud.py
+++ WebAud.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
 import simpleaudio as sa
-
-
+import re
+import random
+import os
 
 app = Flask( __name__ )
 
@@ -10,8 +11,17 @@ def hello():
     if request.method == 'POST':
         if request.form['shush'] == 'shush':
             print( "We doin summit" )
-            wave_obj = sa.WaveObject.from_wave_file("test.wav")
-            play_obj = wave_obj.play()
-            play_obj.wait_done()
+            audioFiles = os.listdir( './audio' )
+            audioFiles = [ a for a in audioFiles if re.match( r'.*\.wav', a, re.IGNORECASE ) ]
+            randL = random.sample( audioFiles, 1 )
+            audioPath = randL[ 0 ]
+            print( audioPath )
+            try:
+                wave_obj = sa.WaveObject.from_wave_file( os.path.join( "./audio", audioPath ) )
+                play_obj = wave_obj.play()
+                play_obj.wait_done()
+            except:
+                print( "No luck...." )
+                
 
     return render_template('index.html')
